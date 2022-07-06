e0b6d08e0d3fa3ef76b29348e99c5e674f9bddc9
Roomysteve12 <54193494+Roomysteve12@users.noreply.github.com>
2022-07-06 17:58:26 +0800

    Add files via upload

---
+++ commands/minecraft/mcinfo.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = {
+  name: "mcinfo",
+run: async (client, message, args, config) => {
+
+ let server = await fetch(`https://api.mcsrvstat.us/bedrock/2/${config.ip}:${config.port}`).then(response => response.json());
+
+if(!config.software === 'bedrock' && !config.software === 'java') return;
+
+if(config.software === 'bedrock') {
+  
+let embed = new Discord.MessageEmbed()
+
+  .setThumbnail(message.guild.iconURL())
+  .setColor('RANDOM')
+  .setTitle('<:grass:958700079529721896> | Minecraft Server Status')
+  .addField('IP Address:', `${config.ip}`, true)
+  .addField('Port:', `${config.port}`, true)
+  .addField('Software:', `${config.version}`)
+  .addField('Version:', `${server.version}`)
+  .setFooter({ text: 'Created By Roomysteve12#1152' })
+
+  message.reply({ embeds: [embed] })
+}
+
+if(config.software === 'java') {
+  
+let embed = new Discord.MessageEmbed()
+
+  .setThumbnail(message.guild.iconURL())
+  .setColor('RANDOM')
+  .setTitle('<:grass:958700079529721896> | Minecraft Server Status')
+  .addField('IP Address:', `${config.ip}`, true)
+  .addField('Port:', `${config.port}`, true)
+  .addField('Software:', `Java`)
+  .addField('Version:', `${server.version}`)
+  .setFooter({ text: 'Created By Roomysteve12#1152' })
+
+  message.reply({ embeds: [embed] })
+}  
+}
+}
