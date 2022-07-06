1c5785cad4266a9b9ffd52897e5d9a7311b50539
Roomysteve12 <54193494+Roomysteve12@users.noreply.github.com>
2022-07-06 17:36:55 +0800

    Added Files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Minecraft-Server-Status-Discord-Bot
+
+## ABOUT US
+<p align="center">
+   <img src="https://github.com/Roomysteve12/Minecraft-Server-Status-Discord-Bot/blob/main/center.png"/>
+</p>
+
+**DiscordJS**
+
+- **[LANGUAGE: NodeJS](https://en.wikipedia.org/wiki/Node.js)** 
+- **[Packages](https://github.com/Roomysteve12/Minecraft-Server-Status-Discord-Bot/blob/main/package.json)**
---
+++ index.js
@@ -0,0 +1,164 @@
+// Checker
+const colors = require('colors')
+const config = require('./config.json')
+const software = ['java', 'bedrock']
+if(!config.prefix || !config.token || !config.software || !config.ip || !software.includes(config.software)) {
+  return console.log(`Go To config.json file! \nPrefix: ${config.prefix || '!'}\nToken: ${config.token || 'private'}\nSoftware: ${config.software || 'bedrock'}\nIp And Port: ${config.ip || 'play.example.net'}:${config.port || '19132'}`.red)
+} else {
+ console.log('Successfully!'.green)
+}
+
+if(config.software === 'bedrock') {
+  if(!config.port) config.port = 19132
+}
+if(config.software === 'java') {
+  if(!config.port) config.port = 25565
+}
+
+// 24/7 Uptime
+
+const express = require('express')
+const app = express()
+app.listen('3000', () => {
+  console.log('Port: 3000'. green)
+})
+
+app.get('/', (req, res) => {
+  res.send('lol')
+})
+
+// Main JS
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: 32767 })
+const fs = require('fs')
+const SimplDB = require('simpl.db')
+const db = new SimplDB()
+
+const {
+  setIntervalAsync,
+  clearIntervalAsync
+} = require('set-interval-async/dynamic')
+
+client.commands = new Discord.Collection();
+
+['command'].forEach(handler => {
+	require(`./handlers/${handler}`)(client);
+});
+
+client.on('ready', async () => {
+
+  setIntervalAsync(
+  async () => {
+
+    if(config.software === 'bedrock') {
+      const fetch = require('node-fetch')
+
+      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/bedrock/2/${config.ip}:${config.port}`).then(response => response.json())
+      if(debug.query === false) {
+      console.log('Server Not Found!'.red)
+      process.exit()
+      }
+
+      if(!online && online === false) {
+        client.user.setPresence({ activities: [{ name: `${motd.raw} | Status: Offline` }]  })
+      }
+
+      if(online === true) {
+        client.user.setPresence({ activities: [{ name: `${motd.raw} | Status: Online | Players: ${players.online}/${players.max}` }]  })
+      }
+    }
+
+    if(config.software === 'java') {
+      const fetch = require('node-fetch')
+
+      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/2/${config.ip}:${config.port}`).then(response => response.json())
+      if(debug.ping === false) {
+      console.log('Server Not Found!')
+      process.exit()
+      }
+
+      if(!online && online === false) {
+        client.user.setPresence({ activities: [{ name: `${motd.raw} | Status: Offline` }]  })
+      }
+
+      if(online === true) {
+        client.user.setPresence({ activities: [{ name: `${motd.raw} | Status: Online | Players: ${players.online}/${players.max}` }]  })
+      }
+    }
+  }, 5000) 
+
+  setIntervalAsync(
+  async () => {
+
+    if(config.software === 'bedrock') {
+      const fetch = require('node-fetch')
+
+      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/bedrock/2/${config.ip}:${config.port}`).then(response => response.json());
+      if(debug.query === false) {
+      console.log('Server Not Found!'.red)
+      process.exit()
+      }
+      
+      let uptime = db.get('up')
+
+      if(!online && online === false) {
+        if(uptime === 100) return;
+        if(!uptime) db.set('up', 100)
+        if(uptime) db.add('up', 1)
+      }
+
+      if(online === true) {
+        if(uptime === 100) return;
+        if(!uptime) db.set('up', 100)
+        if(uptime) db.subtract('up', 1)
+      }
+    } 
+    
+    if(config.software === 'java') {
+      const fetch = require('node-fetch')
+
+      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/2/${config.ip}:${config.port}`).then(response => response.json());
+      if(debug.ping === false) {
+      console.log('Server Not Found!'.red)
+      process.exit()
+      }
+      let uptime = db.get('up')
+
+      if(!online && online === false) {
+        if(uptime === 100) return;
+        if(!uptime) db.set('up', 100)
+        if(uptime) db.add('up', 1)
+      }
+
+      if(online === true) {
+        if(uptime === 100) return;
+        if(!uptime) db.set('up', 100)
+        if(uptime) db.subtract('up', 1)
+      }
+    }
+  }, 5000)
+})
+
+client.on('messageCreate', async message => {
+  if (!message.guild) return;
+  if(!message.content.startsWith(config.prefix)) return;
+  
+	if (!message.member)
+		message.member = await message.guild.fetchMember(message);
+  
+	const args = message.content
+		.slice(config.prefix.length)
+		.trim()
+		.split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd.length === 0) return;
+
+	let command = client.commands.get(cmd)
+
+  if (command) command.run(client, message, args, db, config);
+})
+
+client.login(config.token).catch(err => {
+  console.error('Invalid Token!'.red)
+})
