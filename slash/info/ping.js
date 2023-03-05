47123c4e51694ff4095bab3e6bbd2b4f1bf40320
Roomy12 <54193494+Roomysteve12@users.noreply.github.com>
2023-03-05 18:33:34 +0800

    Create ping.js

---
+++ slash/info/ping.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+
+module.exports = {
+  name: 'ping',
+  description: 'Check Discord Bot Ping!',
+run: async (client, inter, config, db) => {
+  let embed = new Discord.EmbedBuilder()
+
+      .setThumbnail(inter.guild.iconURL())
+      .setColor(0xFF0033)
+      .setTitle(`${config.emoji || ":skull:"} | Ping`)
+      .setDescription(`${Math.floor(int.client.ws.ping)}ms`)
+      .setFooter({ text: 'Created By Roomy12#9873'})
+
+      inter.reply({ embeds: [embed], ephemeral: true })
+}
+}
