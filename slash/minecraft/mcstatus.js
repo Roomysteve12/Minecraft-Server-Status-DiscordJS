a30c9797d031b5f311adc1fd658c9a03a5347a95
Roomy12 <54193494+Roomysteve12@users.noreply.github.com>
2023-03-05 18:35:16 +0800

    Update mcstatus.js

--- slash/minecraft/mcstatus.js
+++ slash/minecraft/mcstatus.js
@@ -9,18 +9,18 @@ run: async (client, inter, config, db) => {
   let uptime = db.get('up')
 
   if(config.software === 'bedrock') {
-   let { motd, players, online, version } = await fetch(`https://api.mcsrvstat.us/bedrock/2/${config.ip}:${config.port}`).then(response => response.json()) 
+   let { motd, players, online, version } = await fetch(`https://api.mcsrvstat.us/bedrock/2/${config.ip}:${config.port}`).then(response => response.json())
 
     if(online === false) {
       let embed = new Discord.EmbedBuilder()
 
       .setThumbnail(inter.guild.iconURL())
       .setColor(0xFF0033)
-      .setTitle('<:grass:958700079529721896> | Minecraft Server Status')
+      .setTitle(`${config.emoji || ":skull:"} | Minecraft Server Status`)
       .addFields({ name: 'Status: ', value: ':red_circle: Offline' })
-      .setFooter({ text: 'Created By Roomy12#6360'})
+      .setFooter({ text: 'Created By Roomy12#9873'})
 
-      inter.reply({ embeds: [embed] })
+      inter.reply({ embeds: [embed], ephemeral: true })
     }
 
     if(online == true) {
@@ -28,12 +28,12 @@ run: async (client, inter, config, db) => {
 
       .setColor(0x99FF00)
       .setThumbnail(inter.guild.iconURL())
-      .setTitle(`<:grass:958700079529721896> | ${motd.raw}'s Status`)
+      .setTitle(`${config.emoji || ":skull:"} | ${inter.guild.name}'s Status`)
       .addFields({ name: 'Status: ', value: ':green_circle: Online', inline: true })
-      .addFields({ name: 'Players/Max: ', value: `${players.online}/${players.max}`, inline: true })
+      .addFields({ name: 'Players/Max: ', value: `${player.online}/${player.max}`, inline: true })
       .addFields({ name: 'Version: ', value: `${version}`, inline: true})
       .addFields({ name: 'Uptime (Not Accurate): ', value: `${uptime}%`, inline: true})
-      .setFooter({ text: 'Created By Roomy12#6360'})
+      .setFooter({ text: 'Created By Roomy12#9873'})
 
       inter.reply({ embeds: [embed] })
     }
@@ -41,19 +41,22 @@ run: async (client, inter, config, db) => {
 
   if(config.software === 'java') {
    let { motd, players, online, version, info } = await fetch(`https://api.mcsrvstat.us/2/${config.ip}:${config.port}`).then(response => response.json()) 
-    if(!info) info = "null"
-    if(!players.list) players.list = "null"
+    
+   let player = players
+   if(!player) player = "null"
+   if(!info) info = "null"
+   if(!player.list) player.list = "No Players"
 
     if(online === false) {
       let embed = new Discord.EmbedBuilder()
 
       .setThumbnail(inter.guild.iconURL())
       .setColor(0xFF0033)
-      .setTitle('<:grass:958700079529721896> | Minecraft Server Status')
+      .setTitle(`${config.emoji || ":skull:"} | Minecraft Server Status`)
       .addFields({ name: 'Status: ', value: ':red_circle: Offline'})
-      .setFooter({ text: 'Created By Roomy12#6360'})
+      .setFooter({ text: 'Created By Roomy12#9873'})
 
-      inter.reply({ embeds: [embed] })
+      inter.reply({ embeds: [embed], ephemeral: true })
     }
 
     if(online == true) {
@@ -61,13 +64,13 @@ run: async (client, inter, config, db) => {
 
       .setColor(0x99FF00)
       .setThumbnail(inter.guild.iconURL())
-      .setTitle(`<:grass:958700079529721896> | ${motd.raw}'s Status`)
+      .setTitle(`${config.emoji || ":skull:"} | ${inter.guild.name}'s Status`)
       .addFields({ name: 'Status: ', value: ':green_circle: Online', inline: true})
-      .addFields({ name: 'Players/Max: ', value: `${players.online}/${players.max}`, inline: true})
-      .addFields({ name: 'Players: ', value: `${players.list}`})
+      .addFields({ name: 'Players/Max: ', value: `${player.online}/${player.max}`, inline: true})
+      .addFields({ name: 'Players: ', value: `${player.list}`})
       .addFields({ name: 'Version: ', value: `${version}`, inline: true})
       .addFields({ name: 'Uptime (Not Accurate): ', value: `${uptime}%`, inline: true})
-      .setFooter({ text: 'Created By Roomy12#6360'})
+      .setFooter({ text: 'Created By Roomy12#9873'})
 
       inter.reply({ embeds: [embed] })
     }
