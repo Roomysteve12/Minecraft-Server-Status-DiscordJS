d79d2e5c7ede989f86051cf25058795aafbd87ed
Roomy12 <54193494+Roomysteve12@users.noreply.github.com>
2023-03-11 15:29:52 +0800

    Fixed Error

--- slash/info/ping.js
+++ slash/info/ping.js
@@ -9,7 +9,7 @@ run: async (client, inter, config, db) => {
       .setThumbnail(inter.guild.iconURL())
       .setColor(0xFF0033)
       .setTitle(`${config.emoji || ":skull:"} | Ping`)
-      .setDescription(`${Math.floor(int.client.ws.ping)}ms`)
+      .setDescription(`${Math.floor(inter.client.ws.ping)}ms`)
       .setFooter({ text: 'Created By Roomy12#9873'})
 
       inter.reply({ embeds: [embed], ephemeral: true })
