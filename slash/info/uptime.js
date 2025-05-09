8906999868eab1fb2542ad249a08e63c0e12f597
Roomy12 <54193494+Roomysteve12@users.noreply.github.com>
2025-05-09 08:18:27 +0800

    Update uptime.js

--- slash/info/uptime.js
+++ slash/info/uptime.js
@@ -1,20 +1,26 @@
 const Discord = require('discord.js')
-const ms = require("parse-ms");
 
 module.exports = {
   name: 'uptime',
   description: 'Check Discord Bot Uptime!',
 run: async (client, inter, config, db) => {
 
-   let time = ms(client.uptime)
+  function formatDuration(ms) {
+  let seconds = Math.floor((ms / 1000) % 60),
+      minutes = Math.floor((ms / (1000 * 60)) % 60),
+      hours = Math.floor((ms / (1000 * 60 * 60)) % 24),
+      days = Math.floor(ms / (1000 * 60 * 60 * 24));
+
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+}
   
    let embed = new Discord.EmbedBuilder()
 
       .setThumbnail(inter.guild.iconURL())
       .setColor(0xFF0033)
       .setTitle(`${config.emoji || ":skull:"} | Uptime`)
-      .setDescription(`${time.days} days, ${time.hours} hours, ${time.minutes} minutes, ${time.seconds} seconds`)
-      .setFooter({ text: 'Created By Roomy12#9873'})
+      .setDescription(`${formatDuration(client.uptime)}`)
+      .setFooter({ text: 'Created By _roomysteve12'})
 
       inter.reply({ embeds: [embed], ephemeral: true })
 }
